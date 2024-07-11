ba38e14f1dc80a52bc0e8192d45405aa79179185
MarshTheBacca <marshallhunt@live.com>
2024-07-11 16:30:29 +0100

    added conversions from string to coord and vice versa

--- utils/other_utils.py
+++ utils/other_utils.py
@@ -22,3 +22,22 @@ def letters_to_index(letters: str) -> int:
     for letter in letters:
         index = index * 26 + (ord(letter.upper()) - ord("A") + 1)
     return index - 1
+
+
+def coord_to_string(coord: tuple[int, int]) -> str:
+    col_num = coord[0]
+    col_parts = []
+    while col_num >= 0:
+        col_parts.append(chr(col_num % 26 + ord('A')))
+        col_num = col_num // 26 - 1
+    col_str = ''.join(reversed(col_parts))
+    return f"{coord[1] + 1}{col_str}"
+
+
+def string_to_coord(string: str) -> tuple[int, int]:
+    row_str = ''.join(filter(str.isdigit, string))
+    col_str = ''.join(filter(str.isalpha, string))
+    row_num = int(row_str) - 1
+    col_num = sum((ord(char.upper()) - ord('A') + 1) * (26 ** i)
+                  for i, char in enumerate(reversed(col_str))) - 1
+    return (col_num, row_num)
